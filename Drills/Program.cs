cb0411a0eec0d8fcc008bc85a3d7842db8dca1d1
Jordan Millett <JelloJordan3@gmail.com>
2024-03-31 15:45:43 -0700

    Drills

---
+++ Drills/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Drill Game = new Alphabet();
+
+            Game.Play();
+        }
+    }
+}
